c633a3ec15ab13948aa72e18aed1cf2d5eb8e8bf
jinmc <jinmo55555@gmail.com>
2021-08-27 10:53:37 +0900

    book list with reducers with mapStateToProps

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: "javascript book"},
+        {title: "philosophy book"},
+        {title: "physics book"},
+        {title: "coding book"}
+    ]
+}
