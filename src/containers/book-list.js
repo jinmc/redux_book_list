3c7744706245fc2cb7f0833f0d167cd65e13e001
jinmc <jinmo55555@gmail.com>
2021-08-27 14:49:04 +0900

    make console log when clicked books

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class Booklist extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li 
+                    onClick={() => this.props.selectBook(book)}
+                    key={book.title} 
+                    className="list-group-item">
+                        {book.title}
+                </li>
             );
         });
     }
@@ -26,4 +33,12 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(Booklist);
+// Anything returned from this function will end up as props on the Booklist container
+function mapDispatchToProps(dispatch) {
+    // whatever is returned will show up as props inside of booklist
+    return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+
+// Promote Booklist form a component to a container - it need to know about this new dispatch method, selectBook.
+// Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(Booklist);
